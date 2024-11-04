45af40464e513d79695ee1eebdd0d71cc0d0825d
JangWooJin1 <jangwoojin1@gmail.com>
2024-11-04 16:16:17 +0900

    chore: first project setting

---
+++ src/main/java/pda5th/backend/theOne/TheOneApplication.java
@@ -0,0 +1,13 @@
+package pda5th.backend.theOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pda5th/backend/theOne/TheOneApplicationTests.java
@@ -0,0 +1,13 @@
+package pda5th.backend.theOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
